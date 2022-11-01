5cea2ff7844c640e5c0cb752f7323caf08eeb06a
Дамир <damir.obuhov@mail.ru>
2022-11-02 00:51:13 +0400

    ДЗ 7 задача 1

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        byte car=4;
+        short house=2;
+        int c=6;
+        float r=9;
+        long q=4;
+        double t=7;
+        System.out.println("авто " + car );
+        System.out.println(c);
+        System.out.println(r);
+        System.out.println(q);
+        System.out.println(t);
+    }
+}
